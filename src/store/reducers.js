5722e33b172bdac74bc9d2853f6cee6974fd0687
Alan Ramsay <alan@fanduel.com>
2016-04-26 16:06:18 +0100

    WIP: assigning mines

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -13,7 +13,7 @@ function cells(state = [], action) {
 const cellDefault = { isMine: false, isFlagged: false, isRevealed: false };
 
 function cell(state = cellDefault, action) {
-	return state;
+	return _.assign({}, state);
 }
 
 function currentGameSize(state = gameSizes[0], action) {
@@ -44,6 +44,11 @@ export default combineReducers({
 
 function generateGridFromSize(gameSize) {
 	let gridArray = _.fill(_.range(0, (gameSize.width * gameSize.height)), cell());
+	console.log(gameSize.mines);
+
+	_.each(_.sampleSize(gridArray, gameSize.mines), function(cell) {
+		cell.isMine = true;
+	});
 
 	return gridArray;
 }
